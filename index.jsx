3fce24a0d5aad06166b6d745af359c2cfb6ad772
zhouyunge <zhouyunge@kuaishou.com>
2022-04-16 20:05:15 +0800

    feat：support image

--- index.jsx
+++ index.jsx
@@ -1,12 +1,44 @@
 /** @jsx createElement */
 import {
   createElement,
-  render
+  render,
+  useRef,
+  atom
 } from 'axii'
 
 import Editor from './lib/editor'
 
+function Index () {
+  const editorRef = useRef()
+  const output = atom('')
+
+  return (
+    <index block block-width="1000px" flex-display style={{ gap: '20px' }} >
+      <Editor ref={editorRef} />
+      <output block block-width="600px" style={{ border: '1px solid #333'}}>
+        <button type="button" onClick={() => {
+          output.value = JSON.stringify(editorRef.current.editor.getJSON(), null, 2);
+        }}>json</button>
+        <button type="button" onClick={() => {
+          output.value = editorRef.current.editor.getHTML()
+        }}>html</button>
+        <button type="button" onClick={() => {
+          output.value = editorRef.current.editor.getText()
+        }}>text</button>
+        <button type="button" onClick={() => {
+          output.value = JSON.stringify(editorRef.current.editor.getAttributes(), null, 2);
+        }}>attributes</button>
+        <pre>
+          <code >
+            {() => output.value}
+          </code>
+        </pre>
+      </output>
+    </index>
+  )
+}
+
 render(
-  <Editor />,
+  <Index />,
   document.getElementById('editor')
 )
--- lib/editor.jsx
+++ lib/editor.jsx
@@ -7,36 +7,75 @@ import {
 } from "axii"
 import { Editor } from '@tiptap/core'
 import StarterKit from '@tiptap/starter-kit'
+import Image from '@tiptap/extension-image'
+import Link from '@tiptap/extension-link'
 import './editor.less'
+import IP from './image-paste'
 
-function EditorFC () {
+function EditorFC ({ ref }) {
   const tipEditorEle = useRef()
+
+  const tipEditor = useRef()
+
+  if (ref) {
+    ref.current = {
+      get editor () {
+        return tipEditor.current
+      }
+    }
+  }
 
   useViewEffect(() => {
     if(tipEditorEle.current) {
-      new Editor({
+      tipEditor.current = new Editor({
         element: tipEditorEle.current,
         extensions: [
-          StarterKit
+          IP,
+          Object.assign(StarterKit, ({
+            options: {
+              italic: false,
+              bold: false,
+              code: false,
+            }
+          })),
+          Image,
+          Link
         ],
         content: `
           <p>hello</p>
           wo
+          <img src="https://cdn.233.momobako.com/ygopro/pics/21844576.jpg!half" />
         `
       })
+      console.log('tipEditor.current:', tipEditor.current)
+      window.tipEditor = tipEditor.current
     }
   })
 
+  function dropImage (e) {
+    console.log('e: ', e);
+  }
+
   return (
     <dwEditorContainer block >
-      dw Editor
+      dw Editor      
       
-      <tipEditor ref={tipEditorEle}>
-
+      <tipEditor block ref={tipEditorEle} onDragover={e => {
+        e.preventDefault()
+        e.dataTransfer.dropEffect = 'copy'
+      }} onDrop={dropImage} > 
       </tipEditor>
     </dwEditorContainer>
   )
 }
+
+EditorFC.Style = (frag) => {
+  const el = frag.root.elements
+  el.tipEditor.style({
+    border: '1px solid #999'
+  })
+}
+EditorFC.forwardRef = true
 
 const MyEditor = createComponent(EditorFC)
 
